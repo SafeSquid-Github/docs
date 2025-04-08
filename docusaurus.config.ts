c030a5c7c40c2ecfdb9fec54052c2c1fa584cdb9
highbrowintown <pratikpoojary124@outlook.com>
2025-04-08 08:17:00 +0000

    fix: update production URL in docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'http://10.200.2.121',
+  url: 'http://docsnew.safesquid.com'
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
